1c34b461463db12941f8464b214f057ae6146bfa
MrYurihi <mryurihi2003@gmail.com>
2017-06-22 06:37:55 -0400

    Add files via upload

---
+++ spec/mcfunction-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import McfunctionView from '../lib/mcfunction-view';
+
+describe('McfunctionView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
